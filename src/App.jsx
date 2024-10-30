52e726ff25ef906c45ff8ebc7f4cb43b9c7af6ff
Md-sajeeb001 <mohammadsajeeb890@email.com>
2024-10-30 21:34:57 +0800

    explor using from in deffrent way simpleFrom stateFrom RefFrom

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import RefForm from "./Components/RefForm/RefForm";
+// import StateForm from "./Components/StateForm/StateForm";
+// import SimpleForm from "./Components/SimpelForm/SimpleForm";
+
+function App() {
+  return (
+    <>
+      <h1>Vite + React</h1>
+      {/* <SimpleForm></SimpleForm> */}
+      {/* <StateForm></StateForm> */}
+      <RefForm></RefForm>
+    </>
+  );
+}
+
+export default App;
